7f8ef14c943635cdbcd093ff7ad4a76227f8b477
Wajahat Rasool <wajahatrasool2003@gmail.com>
2026-03-29 09:44:53 +0500

    docker-compose file is updated

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,3 +1,4 @@
 def call(){
-  sh "docker-compose down && docker-compose up -d"
+  // Hyphen hata dein warna agent par command not found ka error aayega
+  sh "docker compose down && docker compose up -d"
 }
